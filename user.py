eb5fd44bd45f392c6136261182e8f82306f5ef58
Norah.Waswala <norah.waswala@student.moringa.com>
2022-04-26 00:56:49 +0300

    refactor: remove the dispaly functionality from the class

--- user.py
+++ user.py
@@ -25,8 +25,21 @@ class User:
         User.user_list.remove(self)
 
     @classmethod
+    def verify_user(cls,username, password):
+        """
+        method to verify whether the user is in our user_list or not
+        """
+        a_user = ""
+        for user in User.user_list:
+            if(user.username == username and user.password == password):
+                    a_user == user.username
+        return a_user
+
+
+    @classmethod
     def display_user(cls): 
         '''
         method that returns the user list
         '''
         return cls.user_list
+    
