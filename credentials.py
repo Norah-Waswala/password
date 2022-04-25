0556387804ba75d21a11a133fa3a5cec6d9c7143
Norah.Waswala <norah.waswala@student.moringa.com>
2022-04-26 00:55:20 +0300

    feat: add new account functionality in credentials class

--- credentials.py
+++ credentials.py
@@ -1,3 +1,4 @@
+import string
 import pyperclip
 import random
 class credentials:
@@ -27,26 +28,26 @@ class credentials:
         credentials.credentials_list.remove(self)
 
     @classmethod
-    def find_by_account_username(cls,string):
+    def find_credential(cls,account):
         for credentials in cls.credentials_list:
-            if credentials.account_username == string:
+            if credentials.account_username == account:
                 return credentials
 
                             
     @classmethod
-    def credentials_exist(cls,string):
+    def credentials_exist(cls,account):
         """checks if a credential exists
 
         Args:
             string (string): account_name 
         """        
         for credentials in cls.credentials_list:
-            if credentials.account_name == string:
+            if credentials.account_name == account:
                 return True
         return False
 
     @classmethod
-    def display_credentialss(cls):
+    def display_credentials(cls):
         '''
         method that returns the credentials list
         '''
@@ -57,19 +58,22 @@ class credentials:
     #     credentials_found = credentials.find_by_account_username(string)
     #     pyperclip.copy(credentials_found.account_username)
 
-       
+    def generate_Password(stringLength=8):
+        """Generate a random password string of letters and digits and special characters"""
+        password = string.ascii_uppercase + string.ascii_lowercase + string.digits + "~!@#$%^&*"
+        return ''.join(random.choice(password) for i in range(stringLength))
+    # def generate_passward(cls,string):
+    #     chars="abcdefghijklmnopqrstuvwxyz123456789#@$*&!"
 
-chars="abcdefghijklmnopqrstuvwxyz123456789#@$*&!"
-
-while 1:
-    passward_len=int(input("what length would you want your passward to be:"))
-    passward_count=int(input("how many passwards do you need:"))
-    for x in range(0,passward_count):
-        passward=""
-        for x in range(0,passward_len):
-            passward_char=random.choice(chars)
-            passward     =passward+passward_char
-        print("Here is your passward : ",passward)
+    #     while 1:
+    #         passward_len=int(input("what length would you want your passward to be:"))
+    #         # passward_count=int(input("how many passwards do you need:"))
+    #         for x in range(0,):
+    #             passward=""
+    #         for x in range(0,passward_len):
+    #             passward_char=random.choice(chars)
+    #         passward     =passward+passward_char
+            
 
 
 
